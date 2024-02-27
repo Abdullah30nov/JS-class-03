e71c959c30c6a610b7451981d03b1cc1fae6ac57
abdullah30nov <malikshoaibawan890malik@gmail.com>
2024-02-27 21:10:41 +0500

    Js-class-03

---
+++ app.js
@@ -0,0 +1,64 @@
+// var a = 3;
+// if(a > 5){
+//     alert("This is greater value");
+// }
+// else if( a < 5 ){
+//     alert("This is not grater value");
+// }
+// else{
+//     alert("not sure");
+// }
+
+// var a = +prompt("Enter Value");
+// if (a > 500){
+//     alert("This is 500+ value");
+// }
+// else if (a < 500){
+//     alert("This is 500- Value");
+// }
+// else {
+//     alert("This is correct value");
+// }
+
+
+
+
+
+
+// var m = +prompt("Enter Mathematics mark");
+// var u = +prompt("Enter Urdu mark");
+// var c = +prompt("Enter Chemistry mark");
+// var b = +prompt("Enter Biology mark");
+
+// var total = (m + u + c + b)/400*100;
+//        alert(total);
+// var perc = +prompt("Enter total Percentage");
+// if (perc >= 80 && perc <100){
+//     alert("A+");
+// }
+// else if (perc >= 70 && perc < 80){
+//     alert("A");
+// }
+// else if (perc >= 60 && perc < 70){
+//     alert("B");
+// }
+// else if (perc >= 50 && perc < 60){
+//     alert("C");
+// }
+// else if (perc >= 40 && perc < 50){
+//     alert("D");
+// }
+// else if (perc >= 33 && perc < 40){
+//     alert("E");
+// }
+// else if (perc >= 0 && perc < 33){
+//     alert("Fail");
+// }
+// else{
+//     alert("Not Sure");
+// }
+
+
+
+
+
